5e5ba8c51b8acfc46edb4d718850514f7e6f7b2c
MTruong2000 <truongdev2000@gmail.com>
2026-04-07 20:55:24 +0700

    update error cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://wannapet-house-ui.vercel.app/"],
+    origin: ["http://localhost:3000", "https://wannapet-house-ui.vercel.app"],
     credentials: true,
   })
 );
